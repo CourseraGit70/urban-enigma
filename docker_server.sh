0f3a921213f6161946bc6713d2396521dd7db9ae
Jacob Baker <47368122+baker-cu@users.noreply.github.com>
2019-11-24 16:33:48 -0800

    Update and rename docker_server to docker_server.sh

--- docker_server.sh
+++ docker_server.sh
@@ -8,3 +8,5 @@ docker run -v $PWD/vpn-data:/etc/openvpn --rm -it myownvpn ovpn_initpki
 docker run -v $PWD/vpn-data:/etc/openvpn -d -p 3000:1194/udp --cap-add=NET_ADMIN myownvpn
 docker run -v $PWD/vpn-data:/etc/openvpn --rm -it myownvpn easyrsa build-client-full user1 nopass
 docker run -v $PWD/vpn-data:/etc/openvpn --rm myownvpn ovpn_getclient user1 > user1.ovpn
+
+docker create --name ue-server myownvpn
