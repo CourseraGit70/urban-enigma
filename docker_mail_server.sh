e040816d00687ccf5520020a59daa2b31d049186
CourseraGit70 <gtyler@chapman.edu>
2019-11-28 14:09:13 -0800

    Create docker_mail_server.sh

---
+++ docker_mail_server.sh
@@ -0,0 +1,16 @@
+docker pull tvial/docker-mailserver:latest
+curl -o setup.sh https://raw.githubusercontent.com/tomav/docker-mailserver/master/setup.sh; chmod a+x ./setup.sh
+
+curl -o docker-compose.yml https://raw.githubusercontent.com/tomav/docker-mailserver/master/docker-compose.yml.dist
+
+curl -o .env https://raw.githubusercontent.com/tomav/docker-mailserver/master/.env.dist
+
+curl -o env-mailserver https://raw.githubusercontent.com/tomav/docker-mailserver/master/env-mailserver.dist
+
+docker-compose up -d mail
+./setup.sh email add <user@domain> [<password>]
+./setup.sh config dkim
+
+docker-compose down
+docker pull tvial/docker-mailserver:latest
+docker-compose up -d mail
