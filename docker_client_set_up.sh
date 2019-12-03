113cf0a05cac874ffc937a67d12237e29d26ea64
CourseraGit70 <gtyler@chapman.edu>
2019-12-03 13:55:56 -0800

    diverted console output

--- docker_client_set_up.sh
+++ docker_client_set_up.sh
@@ -1,12 +1,12 @@
-docker pull ubuntu
-docker cp ue-server:/testclient.ovpn C:/Users/Jake/DataCom/Urban-Enigma/testclient.ovpn
-docker cp C:/Users/Jake/DataCom/Urban-Enigma/testclient.ovpn ue-client:/etc/openvpn/client
-docker run --name ue-client -i -t ubuntu /bin/bash
-apt-get update
-apt-get upgrade
-apt-get install openvpn
-y
-openvpn –config testclient.ovpn
+docker pull ubuntu >> website/data.js
+docker cp ue-server:/testclient.ovpn C:/Users/Jake/DataCom/Urban-Enigma/testclient.ovpn >> website/data.js
+docker cp C:/Users/Jake/DataCom/Urban-Enigma/testclient.ovpn ue-client:/etc/openvpn/client >> website/data.js
+docker run --name ue-client -i -t ubuntu /bin/bash >> website/data.js
+apt-get update >> website/data.js
+apt-get upgrade >> website/data.js
+apt-get install openvpn >> website/data.js
+y >> website/data.js
+openvpn –config testclient.ovpn >> website/data.js
 
 on the server run the docker.sh file and or server config
 
