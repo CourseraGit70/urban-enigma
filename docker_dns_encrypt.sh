a13f413ca9ab3311a0f80c674c0135fb600d88ce
CourseraGit70 <gtyler@chapman.edu>
2019-12-03 13:56:20 -0800

    Update docker_dns_encrypt.sh

--- docker_dns_encrypt.sh
+++ docker_dns_encrypt.sh
@@ -1,5 +1,5 @@
 docker run --name=dnscrypt-server -p 443:443/udp -p 443:443/tcp --net=host \
 --ulimit nofile=90000:90000 --restart=unless-stopped \
-jedisct1/dnscrypt-server init -N example.com -E 192.168.1.1:443
+jedisct1/dnscrypt-server init -N example.com -E 192.168.1.1:443 >> website/data.js
 
-docker start dnscrypt-server
+docker start dnscrypt-server >> website/data.js
