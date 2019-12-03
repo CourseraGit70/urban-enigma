561648a1b0cf7147711fe276ebee1f287d91eb0f
CourseraGit70 <gtyler@chapman.edu>
2019-12-03 13:57:05 -0800

    Update docker_mail_server.sh

--- docker_mail_server.sh
+++ docker_mail_server.sh
@@ -1,11 +1,11 @@
-docker pull tvial/docker-mailserver:latest
-curl -o setup.sh https://raw.githubusercontent.com/tomav/docker-mailserver/master/setup.sh; chmod a+x ./setup.sh
+docker pull tvial/docker-mailserver:latest >> website/data.js
+curl -o setup.sh https://raw.githubusercontent.com/tomav/docker-mailserver/master/setup.sh; chmod a+x ./setup.sh >> website/data.js
 
-curl -o docker-compose.yml https://raw.githubusercontent.com/tomav/docker-mailserver/master/docker-compose.yml.dist
+curl -o docker-compose.yml https://raw.githubusercontent.com/tomav/docker-mailserver/master/docker-compose.yml.dist >> website/data.js
 
-curl -o .env https://raw.githubusercontent.com/tomav/docker-mailserver/master/.env.dist
+curl -o .env https://raw.githubusercontent.com/tomav/docker-mailserver/master/.env.dist >> website/data.js
 
-curl -o env-mailserver https://raw.githubusercontent.com/tomav/docker-mailserver/master/env-mailserver.dist
+curl -o env-mailserver https://raw.githubusercontent.com/tomav/docker-mailserver/master/env-mailserver.dist >> website/data.js
 
 
 #insert the updated docker-compose.yml file into the directory created
@@ -13,12 +13,12 @@ curl -o env-mailserver https://raw.githubusercontent.com/tomav/docker-mailserver
 
 
 
-docker-compose up -d mail
-./setup.sh email add <user@domain> [<password>]
-./setup.sh config dkim
+docker-compose up -d mail >> website/data.js
+./setup.sh email add <user@domain> [<password>] >> website/data.js
+./setup.sh config dkim >> website/data.js
 
-docker-compose down
-docker pull tvial/docker-mailserver:latest
-docker-compose up -d mail
+docker-compose down >> website/data.js
+docker pull tvial/docker-mailserver:latest >> website/data.js
+docker-compose up -d mail >> website/data.js
 
 
