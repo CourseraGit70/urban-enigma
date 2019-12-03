6b9b046034eab929beed1ab5396436b2fe6832af
meganwalley <44676998+meganwalley@users.noreply.github.com>
2019-12-03 13:29:08 -0800

    Added game files

--- website/alert.js
+++ website/alert.js
@@ -1,3 +1,9 @@
+$(function () {
+	$.get(Typer.file,function(data){
+		Typer.text=data;
+		Typer.text = Typer.text.slice(0, Typer.text.length-1);
+	});
+});
 var Typer={
 	text: null,
 	accessCountimer:null,
@@ -15,11 +21,11 @@ var Typer={
 	},
 
 	content:function(){
-		return $("#console").html();
+		return $("#alert").html();
 	},
 
 	write:function(str){
-		$("#console").append(str);
+		$("#alert").append(str);
 		return false;
 	},
 
@@ -48,7 +54,7 @@ var Typer={
     		else if(Typer.text){
 			var cont=Typer.content();
 			if(cont.substring(cont.length-1,cont.length)=="|")
-				$("#console").html($("#console").html().substring(0,cont.length-1));
+				$("#alert").html($("#alert").html().substring(0,cont.length-1));
 			if(key.keyCode!=8){
 				Typer.index+=Typer.speed;
 			}
@@ -59,7 +65,7 @@ var Typer={
 			var text=Typer.text.substring(0,Typer.index)
 			var rtn= new RegExp("\n", "g");
 
-			$("#console").html(text.replace(rtn,"<br/>"));
+			$("#alert").html(text.replace(rtn,"<br/>"));
 			window.scrollBy(0,50);
 		}
 
@@ -76,7 +82,7 @@ var Typer={
 		var cont=this.content();
 
 		if(cont.substring(cont.length-1,cont.length)=="|")
-			$("#console").html($("#console").html().substring(0,cont.length-1));
+			$("#alert").html($("#alert").html().substring(0,cont.length-1));
 
 		else
 			this.write("|"); // else write it
@@ -98,7 +104,7 @@ function replaceUrls(text) {
 }
 
 Typer.speed=15;
-Typer.file="website/text.txt"; // add your own name here
+Typer.file="website/alert.txt"; // add your own name here
 Typer.init();
 
 var timer = setInterval("t();", 45);
@@ -108,23 +114,4 @@ function t() {
 	if (Typer.index > Typer.text.length) {
 		clearInterval(timer);
 	}
-}
-
-
-function writeTheCookie() {
-  document.cookie = "EASYRSA_REQ_COUNTRY=" + document.getElementById("country").value;
-  document.cookie = "EASYRSA_REQ_PROVINCE=" + document.getElementById("province").value;
-  document.cookie = "EASYRSA_REQ_CITY=" + document.getElementById("city").value;
-  document.cookie = "EASYRSA_REQ_ORG=" + document.getElementById("org").value;
-  document.cookie = "EASYRSA_REQ_EMAIL=" + document.getElementById("email").value;
-  document.cookie = "EASYRSA_REQ_OU=" + document.getElementById("ou").value;
-  document.cookie = "EASYRSA_REQ_UN=" + document.getElementById("username").value;
-  document.cookie = "EASYRSA_REQ_PW=" + document.getElementById("password").value;
-
-	document.getElementById("start_button").value = "Submitted";
-	document.getElementById("start_button").onclick = "";
-}
-
-function resetIFrame() {
-	document.getElementById("game").contentDocument.location.reload(true);
 }
